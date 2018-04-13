e39452291047df2fe26a9bca90370d5048f8c2be
User <User@Users-MacBook-Pro.local>
2018-04-13 09:38:56 +0700

    Initial Commit

---
+++ BoxingTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BoxingTimer
+//
+//  Created by User on 4/13/18.
+//  Copyright © 2018 Dat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
